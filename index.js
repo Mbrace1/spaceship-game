2ac8e13427a5d29f660227af90ae811429b6bae7
Mbrace1 <michaelbracey24@gmail.com>
2022-09-05 16:38:39 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = new PIXI.Application({ width: 640, height: 360 });
+
+document.body.appendChild(app.view);
+
+let sprite = PIXI.Sprite.from('./img/sample.png');
+
+app.stage.addChild(sprite);
+
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+    elapsed += delta;
+    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
